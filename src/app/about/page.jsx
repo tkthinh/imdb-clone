48e4cb969479a201a422581a2b130326ca5bd05a
thinh tu <khangthinhprof@notions.vodanh.edu.vn>
2024-06-21 16:19:25 +0700

    add about page

---
+++ src/app/about/page.jsx
@@ -0,0 +1,43 @@
+import React from "react";
+
+export default function About() {
+  return (
+    <div className="max-w-6xl mx-auto p-3 space-y-4">
+      <h1 className='text-2xl font-medium text-amber-600'>About</h1>
+      <p>
+        Welcome to our IMDB Clone, your go-to source for comprehensive movie, TV
+        show, and celebrity information. Whether you're a casual viewer, a
+        dedicated cinephile, or a media industry professional, our platform
+        provides you with an extensive database of filmography, ratings,
+        reviews, and more. Our mission is to connect movie enthusiasts with the
+        content they love, offering a user-friendly experience that is both
+        informative and engaging.
+      </p>
+      <p>
+        Our platform is designed with you in mind. We offer a wide range of
+        features including detailed profiles for movies and TV shows, complete
+        with cast and crew information, trivia, user reviews, and critic
+        ratings. You can explore upcoming releases, watch trailers, and get
+        recommendations based on your viewing history. Our advanced search
+        functionality allows you to find exactly what you're looking for, from
+        the latest blockbusters to hidden gems and cult classics.
+      </p>
+      <p>
+        Beyond movies and TV shows, we shine a spotlight on the people behind
+        the magic. Our celebrity profiles include comprehensive biographies,
+        filmographies, awards, and news updates, giving you an in-depth look at
+        your favorite actors, directors, writers, and more. We also feature
+        industry news and box office reports to keep you informed about the
+        latest trends and happenings in Hollywood and beyond.
+      </p>
+      <p>
+        Join our community of movie lovers today and dive into the world of
+        cinema like never before. Whether you're here to discover new favorites,
+        learn more about the films and shows you love, or stay updated with the
+        latest entertainment news, our IMDB Clone is your ultimate destination
+        for all things movie and TV. Thank you for choosing us as your trusted
+        source for entertainment information.
+      </p>
+    </div>
+  );
+}
