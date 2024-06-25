2f543cbf2a5c9c5ab578499d6d69b710cbd3ac12
thinh tu <khangthinhprof@notions.vodanh.edu.vn>
2024-06-25 12:48:42 +0700

    add loading effect

---
+++ src/app/loading.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function loading() {
+  return (
+    <div className='flex justify-center mt-16'>
+      <img className='h-64' src="loading.svg" alt="Loading..." />
+    </div>
+  )
+}
